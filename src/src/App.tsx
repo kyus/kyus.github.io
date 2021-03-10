147e2530ce53afdd0467bb695de476e4b299f342
kyus <kyus@aty.kr>
2021-03-10 12:30:17 +0900

    themplate layout ..
    - set theme ( light / dark Mode )
    - Sider toggle

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import { Layout, Menu, Button } from "antd";
 import "./App.scss";
@@ -7,64 +7,101 @@ import {
   MenuUnfoldOutlined,
   TwitterOutlined,
   QqOutlined,
+  AreaChartOutlined,
+  AliwangwangOutlined,
+  AliwangwangFilled,
+  LinkOutlined,
+  SmileFilled,
+  WomanOutlined,
 } from "@ant-design/icons";
 
 function App() {
-  const [viewSide, setViewSide] = useState(false);
+  const [viewSide, setViewSide] = useState(true);
+  const [darkMode, setDarkMode] = useState(false);
   const { Header, Content, Sider, Footer } = Layout;
+
+  const changeTheme = () => {
+    setDarkMode(!darkMode);
+    localStorage.setItem("theme", darkMode ? "lightMode" : "darkMode");
+  };
+
+  const toggleSideBar = () => {
+    setViewSide(!viewSide);
+    localStorage.setItem("sideBar", viewSide ? "closed" : "opened");
+  };
+
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    const sideBar = localStorage.getItem("sideBar");
+    if (theme === "darkMode") {
+      changeTheme();
+    }
+    if (sideBar && sideBar !== "opened") {
+      toggleSideBar();
+    }
+  }, []);
 
   return (
     <div className="App">
       <Layout style={{ minHeight: "100vh" }}>
-        {viewSide && (
-          <Sider>
-            <div style={{ width: "100%" }}>
-              <img
-                src={logo}
-                className="App-logo"
-                alt="logo"
-                style={{ width: "100%" }}
-              />
-            </div>
-            <Menu theme={"dark"}>
-              <Menu.Item>
-                <TwitterOutlined />
-                side1
-              </Menu.Item>
-              <Menu.Item>
-                <QqOutlined />
-                side2
-              </Menu.Item>
-              <Menu.Item>
-                <Button type={"primary"}>
-                  <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Learn React
-                  </a>
-                </Button>
-              </Menu.Item>
-            </Menu>
-          </Sider>
-        )}
+        <Sider
+          theme={darkMode ? "dark" : "light"}
+          breakpoint={"xs"}
+          hidden={!viewSide}
+        >
+          <Menu theme={darkMode ? "dark" : "light"}>
+            <Menu.Item style={{ height: 200 }}>
+              <div style={{ width: "100%" }}>
+                <img
+                  src={logo}
+                  className="App-logo"
+                  alt="logo"
+                  style={{ width: "100%" }}
+                />
+              </div>
+            </Menu.Item>
+            <Menu.Item icon={<TwitterOutlined />}>side1</Menu.Item>
+            <Menu.Item icon={<QqOutlined />}>side2</Menu.Item>
+            <Menu.Item icon={<LinkOutlined />}>
+              <Button type={"primary"}>
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn React
+                </a>
+              </Button>
+            </Menu.Item>
+            <Menu.Item icon={<MenuUnfoldOutlined />} onClick={toggleSideBar}>
+              hide menu
+            </Menu.Item>
+          </Menu>
+        </Sider>
         <Layout>
-          <Header>
-            <Menu theme={"dark"} mode={"horizontal"}>
-              <Menu.Item onClick={() => setViewSide(!viewSide)}>
+          <Header style={{ padding: 0 }}>
+            <Menu theme={darkMode ? "dark" : "light"} mode={"horizontal"}>
+              <Menu.Item onClick={toggleSideBar}>
                 <div>
                   {viewSide ? <MenuFoldOutlined /> : <MenuUnfoldOutlined />}
                 </div>
               </Menu.Item>
-              <Menu.Item>frist</Menu.Item>
-              <Menu.Item>second</Menu.Item>
-              <Menu.Item>third</Menu.Item>
+              <Menu.Item icon={<AreaChartOutlined />}>frist</Menu.Item>
+              <Menu.Item icon={<AliwangwangOutlined />}>second</Menu.Item>
+              <Menu.Item icon={<WomanOutlined />}>third</Menu.Item>
+              <Menu.Item
+                icon={darkMode ? <SmileFilled /> : <AliwangwangFilled />}
+                onClick={changeTheme}
+              >
+                {darkMode ? "lightMode" : "darkMode"}
+              </Menu.Item>
             </Menu>
           </Header>
           <Content>content layer</Content>
-          <Footer>this is footer</Footer>
+          <Footer>
+            <div>Copyright 2021. kyus.All rights reserved.</div>
+          </Footer>
         </Layout>
       </Layout>
     </div>
