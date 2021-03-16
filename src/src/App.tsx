99f2851192ae90bf920289bd33f0b6990ee859ac
kyus <kyus@aty.kr>
2021-03-16 21:21:39 +0900

    구조 변경 ..
    - SideBar 파일 별도 분리
    - location.pathname 구분하여 menu selected 처리 추가
    - 각 function / 컴포넌트  type 정의 보강

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,94 +1,77 @@
-import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
-import Cat from "./cat";
-import { Layout, Menu, Button } from "antd";
+import React, { useState, useEffect, FC } from "react";
+import { Layout, Menu, Switch } from "antd";
 import "./App.scss";
-import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  TwitterOutlined,
-  QqOutlined,
-  AreaChartOutlined,
-  AliwangwangOutlined,
-  AliwangwangFilled,
-  LinkOutlined,
-  GithubOutlined,
-  CommentOutlined,
-} from "@ant-design/icons";
-import classNames from "classnames";
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
 import _ from "lodash";
-import { mainMenu, subMenu, categoryMenu } from "./config/menu";
+import { mainMenu } from "./config/menu";
 import { RouteComponentProps } from "react-router-dom";
-import { Main, About, Post } from "./component";
+import { Main, About, Post } from "./components";
+import { getIcon } from "./lib/utils";
+import { nightIcon, sunIcon } from "./lib/svgIcons";
+import SideMenu from "./SideMenu";
 
 interface postingProps {
   category?: string | undefined;
   postNumber?: string | undefined;
 }
-
 function App(props: RouteComponentProps<postingProps>) {
-  const { history, location, match } = props;
-  const [viewSide, setViewSide] = useState(true);
+  const { history, location } = props;
+  const [viewSide, setViewSide] = useState(false);
   const [darkMode, setDarkMode] = useState(false);
-  const { Header, Content, Sider, Footer } = Layout;
+  const [selectedMenu, setSelectedMenu] = useState("0");
+  const [selectedSideMenu, setSelectedSideMenu] = useState("0");
+  const { Header, Content, Footer } = Layout;
+  const { pathname } = location;
 
-  const changeTheme = () => {
+  const changeTheme = (): void => {
     setDarkMode(!darkMode);
     localStorage.setItem("theme", darkMode ? "lightMode" : "darkMode");
   };
 
-  const toggleSideBar = () => {
+  const toggleSideBar = (): void => {
     setViewSide(!viewSide);
     localStorage.setItem("sideBar", viewSide ? "closed" : "opened");
   };
 
-  const getIcon = (type: string) => {
-    switch (type) {
-      case "main":
-        return <AreaChartOutlined />;
-      case "about":
-        return <AliwangwangOutlined />;
-      case "twitter":
-        return <TwitterOutlined />;
-      case "qq":
-        return <QqOutlined />;
-      case "comment":
-        return <CommentOutlined />;
-      default:
-        return <GithubOutlined />;
-    }
-  };
+  const GetMainMenu: FC = () => {
+    const _setSelectedMenu = (
+      k: number,
+      pathnameArr: string[],
+      link: string[]
+    ): void => {
+      if (k.toString() !== selectedMenu && pathnameArr[1] === link[1]) {
+        setSelectedMenu(k.toString());
+      }
+    };
+    const pathnameArr: string[] = pathname.split("/");
 
-  const getMainMenu = () => {
-    return _.map(mainMenu, (m, k) => {
-      return (
-        <Menu.Item
-          key={k}
-          onClick={() => history.push(m.link)}
-          icon={getIcon(m.icon)}
-        >
-          {m.name}
+    return (
+      <Menu
+        theme={darkMode ? "dark" : "light"}
+        mode={"horizontal"}
+        selectedKeys={[selectedMenu]}
+      >
+        <Menu.Item onClick={toggleSideBar}>
+          <div>{viewSide ? <MenuFoldOutlined /> : <MenuUnfoldOutlined />}</div>
         </Menu.Item>
-      );
-    });
+        {_.map(mainMenu, (m, k) => {
+          const link: string[] = m.link.split("/");
+          _setSelectedMenu(k, pathnameArr, link);
+          return (
+            <Menu.Item
+              key={k}
+              onClick={() => history.push(m.link)}
+              icon={getIcon(m.icon)}
+            >
+              {m.name}
+            </Menu.Item>
+          );
+        })}
+      </Menu>
+    );
   };
 
-  const getSideMenu = (): Array<{
-    name: string;
-    icon: string;
-    link: string;
-  }> => {
-    type menuStructure = { [subLink: string]: Array<any> };
-    const menu: menuStructure = subMenu;
-
-    const { pathname } = location;
-    const subLink = _.get(pathname.split("/"), `${1}`, "main");
-    const key: string = subLink ? subLink : "main";
-    return key === "post" ? categoryMenu : menu[key];
-  };
-
-  const getContent = () => {
-    const { pathname } = location;
+  const GetContents: FC = () => {
     switch (true) {
       case /^\/about/.test(pathname):
         return <About />;
@@ -105,73 +88,48 @@ function App(props: RouteComponentProps<postingProps>) {
     if (theme === "darkMode") {
       changeTheme();
     }
-    if (sideBar && sideBar !== "opened") {
+    if (sideBar && sideBar === "opened") {
       toggleSideBar();
     }
   }, []);
 
-  const sideMenu = getSideMenu();
+  useEffect(() => {
+    _.map(mainMenu, (m) => {
+      if (selectedSideMenu !== "0" && pathname === m.link) {
+        setSelectedSideMenu("0");
+      }
+    });
+  }, [pathname, selectedSideMenu]);
+
+  const sideMenuProps = {
+    darkMode,
+    viewSide,
+    selectedSideMenu,
+    setSelectedSideMenu,
+    toggleSideBar,
+    ...props,
+  };
 
   return (
     <div className="App">
       <Layout style={{ minHeight: "100vh" }}>
-        <Sider
-          theme={darkMode ? "dark" : "light"}
-          breakpoint={"xs"}
-          hidden={!viewSide}
-        >
-          <Menu theme={darkMode ? "dark" : "light"}>
-            <div className={classNames({ darkMode })} style={{ padding: 20 }}>
-              {<Cat />}
-            </div>
-            {_.map(sideMenu, (m, k) => {
-              return (
-                <Menu.Item
-                  key={k}
-                  icon={getIcon(m.icon)}
-                  onClick={() => history.push(m.link)}
-                >
-                  {m.name}
-                </Menu.Item>
-              );
-            })}
-            <Menu.Item icon={<LinkOutlined />}>
-              <Button type={"primary"}>
-                <a
-                  className="App-link"
-                  href="https://github.com/kyus"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  GitHub
-                </a>
-              </Button>
-            </Menu.Item>
-            <Menu.Item icon={<MenuUnfoldOutlined />} onClick={toggleSideBar}>
-              hide menu
-            </Menu.Item>
-          </Menu>
-        </Sider>
+        <SideMenu {...sideMenuProps} />
         <Layout>
           <Header style={{ padding: 0 }}>
-            <Menu theme={darkMode ? "dark" : "light"} mode={"horizontal"}>
-              <Menu.Item onClick={toggleSideBar}>
-                <div>
-                  {viewSide ? <MenuFoldOutlined /> : <MenuUnfoldOutlined />}
-                </div>
-              </Menu.Item>
-              {getMainMenu()}
-              <Menu.Item
-                icon={
-                  darkMode ? <AliwangwangOutlined /> : <AliwangwangFilled />
-                }
-                onClick={changeTheme}
-              >
-                {darkMode ? "lightMode" : "darkMode"}
-              </Menu.Item>
-            </Menu>
+            <GetMainMenu />
+            <div className={"toggleTheme"}>
+              <Switch
+                autoFocus
+                checked={darkMode}
+                onChange={changeTheme}
+                checkedChildren={nightIcon(14, 14)}
+                unCheckedChildren={sunIcon(14, 14)}
+              />
+            </div>
           </Header>
-          <Content>{getContent()}</Content>
+          <Content>
+            <GetContents />
+          </Content>
           <Footer>
             <div>Copyright 2021. kyus.All rights reserved.</div>
           </Footer>
